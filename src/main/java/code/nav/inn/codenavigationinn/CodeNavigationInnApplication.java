7d68d475f0c1ea1b2d4f5eafcc8dbed0f08683a8
Allen-zc <1053831730@qq.com>
2021-08-11 23:38:55 +0800

    Initial commit

---
+++ src/main/java/code/nav/inn/codenavigationinn/CodeNavigationInnApplication.java
@@ -0,0 +1,13 @@
+package code.nav.inn.codenavigationinn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeNavigationInnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeNavigationInnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/code/nav/inn/codenavigationinn/CodeNavigationInnApplicationTests.java
@@ -0,0 +1,13 @@
+package code.nav.inn.codenavigationinn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeNavigationInnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
